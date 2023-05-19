0ea1797b952fabc1e7b8faec732540341abc9809
Mohanad Khaled <67065678+MohanadKh03@users.noreply.github.com>
2023-05-20 02:32:03 +0300

    Create BST.h

---
+++ Binary Search Tree/BST.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+template<class T>
+class Node{
+public:
+    T val;
+    Node<T>*left;
+    Node<T>*right;
+    Node(){
+        this->val = 0;
+        left = right = nullptr;
+    }
+    Node(T val){
+        this->val = val;
+        left = right = nullptr;
+    }
+};
+
+template<class T>
+class BST
+{
+    Node<T>* root;
+    Node<T>* Find(T val);
+
+    void InOrder(Node<T>* root);
+    void PreOrder(Node<T>* root);
+    void PostOrder(Node<T>* root);
+
+    Node<T>* findMin(Node<T>* root);
+
+    void deleteAll(Node<T>*root);
+
+public:
+    BST();
+    void Insert(T val);
+    bool Contains(T val);
+    void Traverse(int traversalWay);
+    void Remove(T val);
+    ~BST();
+};
